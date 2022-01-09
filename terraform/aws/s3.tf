2f4edbd09563807f3bcc8528cea0a1c140092ff7
Steve Vaknin <62605534+SteveVaknin@users.noreply.github.com>
2022-01-09 12:36:59 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -3,11 +3,12 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
+  
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
   force_destroy = true
   tags = {
-    Name        = "${local.resource_prefix.value}-data"
+    Name        = "${local.resource_prefix.value}-data-2"
     Environment = local.resource_prefix.value
   }
 }
